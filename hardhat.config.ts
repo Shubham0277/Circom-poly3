b6fd33f21e472bd04fbd39774d9a9b3bccafcf4f
Shubham0277 <141708942+Shubham0277@users.noreply.github.com>
2023-08-28 14:12:56 +0530

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,35 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-circom";
+import circuits = require('./circuits.config.json')
+
+process.env.BASE_PATH = __dirname;
+
+import "./tasks/newcircuit.ts"
+
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.11",
+      }
+    ]
+  },
+  networks:{
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/amdAP9OM1m_vT_xQk7SyEPZiXNcjViX5`,
+      accounts: ["1c8985eaec7778510abfc441fa478b4cb80f900462e23d20129eae23861c0c8c"]//process.env.private
+    }
+  },
+  circom: {
+    inputBasePath: "./circuits",
+    ptau: "powersOfTau28_hez_final_12.ptau",
+    circuits: JSON.parse(JSON.stringify(circuits))
+  },
+};
+
+export default config;
